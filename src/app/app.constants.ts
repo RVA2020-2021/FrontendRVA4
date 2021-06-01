f30073eb9885a66e37ce3ec1091e30053df8a8ac
DejanaGladic <dejana023@gmail.com>
2021-06-01 11:06:49 +0200

    Deployment 2

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,5 +1,13 @@
+/*stari endpoint-t
 export const ARTIKL_URL = 'http://localhost:8083/artikl';
 export const DOB_URL = 'http://localhost:8083/dobavljac';
 export const POR_URL = 'http://localhost:8083/porudzbina';
 export const STAV_POR_URL = 'http://localhost:8083/stavkaPorudzbine';
-export const STAV_POR_ZA_POR_URL = 'http://localhost:8083/stavkeZaPorudzbinu';
+export const STAV_POR_ZA_POR_URL = 'http://localhost:8083/stavkeZaPorudzbinu';*/
+
+//novi endpoint-i
+export const ARTIKL_URL = 'https://backend-rva4-2-2021.herokuapp.com/artikl';
+export const DOB_URL = 'https://backend-rva4-2-2021.herokuapp.com/dobavljac';
+export const POR_URL = 'https://backend-rva4-2-2021.herokuapp.com/porudzbina';
+export const STAV_POR_URL = 'https://backend-rva4-2-2021.herokuapp.com/stavkaPorudzbine';
+export const STAV_POR_ZA_POR_URL = 'https://backend-rva4-2-2021.herokuapp.com/stavkeZaPorudzbinu';
